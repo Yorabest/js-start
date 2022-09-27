735eacad98708d8ab79a5b7e2c28931e9fcf3d7f
Sophia <melnichenko.cofia07@gmail.com>
2022-09-27 10:00:16 +0300

    dz done

---
+++ js/index-1.js
@@ -0,0 +1,75 @@
+// Частина 1
+
+const str = "Привіт";
+const num = 123;
+const flag = true;
+const txt = "true";
+const nam = "Вам";
+let b = 56;
+let c = false;
+let a = 35;
+let zach = "Навіщо?";
+let fal = true;
+    
+console.log(typeof str);
+console.log(typeof num);
+console.log(typeof flag);
+console.log(typeof txt);
+
+// Частина 2
+// 1
+const userName = prompt("Введіть ім'я");
+console.log(userName);
+
+// 2
+const myAge = 15;
+const friendAge = 14;
+
+alert(myAge + friendAge);
+
+// 3
+console.log(a + b);
+
+// 4
+let a1 = 5 + 3;
+let a2 =  5 - 3;
+let a3 = 5 * 3;
+let a4 = 5/3;
+let a5 = "Данні не надані";
+
+console.log(a1);
+console.log(a2);
+console.log(a3);
+console.log(a4);
+console.log(a5);
+
+// 5
+let a6 = 5 % 3;
+let a7 = 3 % 5;
+let a8 = 5 + '3';
+let a9 ='5' - 3;
+let a10 =  75 + 'кг';
+
+alert(a6);
+alert(a7);
+alert(a8);
+alert(a9);
+alert(a10);
+
+// 6
+const width = 10;
+const height = 28;
+const s = width + height;
+console.log
+
+// 7
+const heightC = 10;
+const dC = 4;
+const v = heightC + dC;
+console.log(v)
+
+// 8
+const n = 3;
+const m = 4;
+const k = Math.sqrt(n **2 + m **2);
+console.log(k) 
